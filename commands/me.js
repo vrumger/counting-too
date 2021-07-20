df61e3430b003862f4a4ec656183052599e3fecd
Andrew Lane <contact@lungers.com>
2021-07-20 14:00:17 -0400

    Allow mentioning someone in the me command

--- commands/me.js
+++ commands/me.js
@@ -5,7 +5,7 @@ const Save = require('../models/save');
 module.exports = {
     name: 'me',
     description: 'Get information about yourself',
-    async execute(message) {
+    async execute(message, args) {
         const channel = await Channel.findOne({
             guildId: message.guild.id,
             channelId: message.channel.id,
@@ -15,9 +15,30 @@ module.exports = {
             return;
         }
 
+        let user = message.author;
+
+        if (
+            args.length > 0 &&
+            args[0].startsWith('<@') &&
+            args[0].endsWith('>')
+        ) {
+            let userId = args[0].slice(2, -1);
+
+            if (userId.startsWith('!')) {
+                userId = userId.slice(1);
+            }
+
+            user = message.client.users.cache.get(userId);
+
+            if (!user) {
+                await message.reply('user not found.');
+                return;
+            }
+        }
+
         let save = await Save.findOne({
             guildId: message.guild.id,
-            userId: message.author.id,
+            userId: user.id,
         });
 
         if (!save) {
@@ -29,7 +50,7 @@ module.exports = {
         const embed = new Discord.MessageEmbed();
 
         embed.setColor('#8965d6');
-        embed.setTitle(`Info for ${message.author.tag}`);
+        embed.setTitle(`Info for ${user.tag}`);
         embed.addField('Saves', save.saves.toFixed(2));
         embed.setFooter(`${message.client.prefix}me`);
 
