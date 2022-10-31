86a02ec69aac2d7f1da4fcd8e490a4ad63c58c2e
Yaakov Schlachter <contact@cgprograms.com>
2022-10-31 21:56:55 +0200

    Added serve highlighting and underdog visibility

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -18,23 +18,49 @@ module.exports = {
                 guildName: channel.guildName,
                 score: channel.getLastNumber(),
             }))
-            .sort((a, b) => b.score - a.score)
-            .slice(0, 10);
+            .sort((a, b) => b.score - a.score);
+        const topGuilds = guilds.slice(0, 10);
 
         const embed = new Discord.MessageEmbed();
 
+        let description = topGuilds
+            .map((guild, index) =>
+                guild.guildName === interaction.guild.name
+                    ? `**${index + 1}. ${
+                          guild.guildName
+                      } - ${new Intl.NumberFormat().format(guild.score)}**`
+                    : `**${index + 1}.** ${
+                          guild.guildName
+                      } - ${new Intl.NumberFormat().format(guild.score)}`,
+            )
+            .join('\n');
+
+        const inTopTen = topGuilds.some(
+            guild => guild.guildName === interaction.guild.name,
+        );
+        if (!inTopTen) {
+            let index = 0;
+            let score = 0;
+
+            for (let i = 0; i < guilds.length; i++) {
+                let guild = guilds[i];
+                if (guild.guildName === interaction.guild.name) {
+                    score = guild.score;
+                    index = i + 1;
+                    break;
+                }
+            }
+
+            description +=
+                '\n**…**\n' +
+                `**${index}. ${
+                    interaction.guild.name
+                } - ${new Intl.NumberFormat().format(score)}**`;
+        }
+
         embed.setColor('#8965d6');
         embed.setTitle('Leaderboard');
-        embed.setDescription(
-            guilds
-                .map(
-                    (guild, index) =>
-                        `**${index + 1}** ${
-                            guild.guildName
-                        } - ${new Intl.NumberFormat().format(guild.score)}`,
-                )
-                .join('\n'),
-        );
+        embed.setDescription(description);
 
         await interaction.reply({
             embeds: [embed],
