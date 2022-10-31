a561f60d897092a131302a9e154b0453701bf2ea
Yaakov Schlachter <contact@cgprograms.com>
2022-10-31 22:44:30 +0200

    Highlight current guild in the leaderboard (#4)

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,6 +1,13 @@
 const Channel = require('../models/channel');
 const Discord = require('discord.js');
 
+const formatGuild = (index, guild, bold) => {
+    const score = new Intl.NumberFormat().format(guild.score);
+    return bold
+        ? `**${index}. ${guild.guildName} - ${score}**`
+        : `**${index}.** ${guild.guildName} - ${score}`;
+};
+
 module.exports = {
     name: 'leaderboard',
     description: 'Show the high score leaderboard',
@@ -15,26 +22,40 @@ module.exports = {
         });
         const guilds = channels
             .map(channel => ({
+                guildId: channel.guildId,
                 guildName: channel.guildName,
                 score: channel.getLastNumber(),
             }))
-            .sort((a, b) => b.score - a.score)
-            .slice(0, 10);
+            .sort((a, b) => b.score - a.score);
+        const topGuilds = guilds.slice(0, 10);
 
         const embed = new Discord.MessageEmbed();
 
+        let description = topGuilds
+            .map((guild, index) =>
+                formatGuild(
+                    index + 1,
+                    guild,
+                    guild.guildName === interaction.guild.name,
+                ),
+            )
+            .join('\n');
+
+        const inTopTen = topGuilds.some(
+            guild => guild.guildName === interaction.guild.name,
+        );
+        if (!inTopTen) {
+            const index = guilds.findIndex(
+                guild => guild.guildId === interaction.guildId,
+            );
+
+            description +=
+                '\n**…**\n' + formatGuild(index + 1, guilds[index], true);
+        }
+
         embed.setColor('#8965d6');
         embed.setTitle('Leaderboard');
-        embed.setDescription(
-            guilds
-                .map(
-                    (guild, index) =>
-                        `**${index + 1}** ${
-                            guild.guildName
-                        } - ${new Intl.NumberFormat().format(guild.score)}`,
-                )
-                .join('\n'),
-        );
+        embed.setDescription(description);
 
         await interaction.reply({
             embeds: [embed],
