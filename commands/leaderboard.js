05a3f8f7cf98709861394379624ba372006f2329
Yaakov Schlachter <contact@cgprograms.com>
2022-11-02 16:44:00 +0200

    Update commands/leaderboard.js
    Co-authored-by: Avrumy <contact@lungers.com>

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -8,7 +8,8 @@ const formatGuild = (index, guild, bold) => {
         : `**${index}.** ${guild.guildName} - ${score}`;
 };
 
-function ordinal_suffix_of(i) {
+// https://stackoverflow.com/a/13627586
+const ordinal = index => {
     let j = i % 10,
         k = i % 100;
     if (j === 1 && k !== 11) {
